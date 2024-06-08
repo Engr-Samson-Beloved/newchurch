3c98e4623182377c810a818e0cd31316495f8bf0
Engr-Samson-Beloved <125021538+Engr-Samson-Beloved@users.noreply.github.com>
2024-06-08 15:49:18 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -8,8 +7,10 @@ for (i = 0; i < acc.length; i++) {
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
+      panel.style.transition = "1s";
     } else {
       panel.style.display = "block";
+      panel.style.transition = "1s";
     }
   });
 };
@@ -21,13 +22,17 @@ function show(){
   var navv = document.getElementById('Navbar');
   var nave = document.getElementById('nave');
   if(isToggled){
-    navv.style.height= "max-content";
+    navv.style.height= "97vh";
     menu.style.visibility = "visible";
     nave.innerHTML = "&times;"
+    navv.style.transition = ".30s";
   }else{
-    navv.style.height= "10vh";
+    navv.style.height= "7vh";
+    navv.style.transition = ".30s";
     menu.style.visibility = "hidden";
-    nave.innerHTML = "&#9776;"
+    nave.innerHTML = "&#9776;";
+
+
   }
 }
 
@@ -47,4 +52,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // Start the typewriter effect
   typeWriter();
+});
+document.addEventListener("DOMContentLoaded", function() {
+  const loader = document.getElementById("loader");
+  const content = document.getElementById("content");
+
+  // Simulate a loading process (e.g., fetching data)
+  setTimeout(() => {
+      loader.style.display = "none";
+      content.style.display = "block";
+  }, 3000); // Adjust the timeout duration as needed
 });
